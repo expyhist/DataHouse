e3cac379ec61ad4b66b27838ad8edd20af59684c
YH_FROM_HOME_HASEE <exceptionalyh@gmail.com>
2021-11-23 22:37:52 +0800

    fix baseUrl

--- client/src/utils/apisSlice.js
+++ client/src/utils/apisSlice.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import config from "config";
+console.log(config);
 export const apisSlice = createApi({
   reducerPath: "datahouse",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: config.baseUrl }),
   tagTypes: ["Config", "Filter", "Demand", "Menu"],
   endpoints: builder => ({
     getMenus: builder.query({
